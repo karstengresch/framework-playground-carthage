ec382f3be2ce5706b74757016179526d7e2ed5c6
Karsten Gresch <git-2015@by-doing.org>
2018-08-29 20:59:56 +0200

    Added Carthage and target and build settings

--- CarthagePlayground/FrameworkPlayground.playground/Contents.swift
+++ CarthagePlayground/FrameworkPlayground.playground/Contents.swift
@@ -1,5 +1,19 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Alamofire
 
-var str = "Hello, playground"
+
+Alamofire.request("https://httpbin.org/get").responseJSON { response in
+  print("Request: \(String(describing: response.request))")   // original url request
+  print("Response: \(String(describing: response.response))") // http url response
+  print("Result: \(response.result)")                         // response serialization result
+  
+  if let json = response.result.value {
+    print("JSON: \(json)") // serialized json response
+  }
+  
+  if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+    print("Data: \(utf8Text)") // original server data as UTF8 string
+  }
+}
